1069c036c0045431ff0924742891441c4469ead7
Vladimir <vladimis1989@mail.ru>
2024-11-23 19:07:23 +0300

    ,

---
+++ asdd/asdd/asddApp.swift
@@ -0,0 +1,17 @@
+//
+//  asddApp.swift
+//  asdd
+//
+//  Created by Vladimir Grishchenkov on 23.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct asddApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ asdd/asddTests/asddTests.swift
@@ -0,0 +1,17 @@
+//
+//  asddTests.swift
+//  asddTests
+//
+//  Created by Vladimir Grishchenkov on 23.11.2024.
+//
+
+import Testing
+@testable import asdd
+
+struct asddTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
